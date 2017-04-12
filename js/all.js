9bbf9e7649c4a740c7b02af3a062b301522ade2e
Anthony Garratt <anthygarratt@aol.de>
2017-04-12 14:10:08 +0200

    Added Menu-Button
    A click on the menu-button opens.. a menu.. containing the users own
    avatar, his username, and a button to change the user-account.

--- js/all.js
+++ js/all.js
@@ -5601,16 +5601,41 @@ class Da_Net extends ZeroFrame {
     }
 
     onRequest(cmd, message) {
+        // console.log("COMMAND", cmd, message)
         if (cmd == "setSiteInfo") {
             this.site_info = message.params // Save site info data to allow access it later
             this.setSiteInfo(message.params)
 
             if (message.params.cert_user_id) {
-                $("#select_user").html(message.params.cert_user_id)
+                this.identicons = this.identicons || {}
+                var asv = parseInt(page.LS.opts.avatar_size.value) || 64
+                if (!this.identicons.hasOwnProperty(asv)) {
+                    this.identicons[asv] = {}
+                }
+                if (!this.identicons.hasOwnProperty(message.params.cert_user_id)) {
+                    var uhash = stringToHex(message.params.cert_user_id).split(' ').join('')
+                    this.identicons[asv][message.params.cert_user_id] = new Identicon(uhash, {
+                        margin: 0.2,
+                        size: asv,
+                        format: 'svg'
+                    }).toString()
+                }
+                var idata = this.identicons[asv][message.params.cert_user_id]
 
-                // this.loadMessages()
-            } else
+                var user_pic_1 = (typeof idata !== "undefined" ? "<img src='data:image/svg+xml;base64," + idata + "' />" : "")
+                var user_pic_2 = '<figure class="avatar" data-initial="' + message.params.cert_user_id.substr(0, 2) + '">' + user_pic_1 + '</figure>'
+
+                $("#select_user").html("Change user")
+                $('#current_user_name').html(message.params.cert_user_id)
+                $("#current_user_avatar").html(user_pic_2)
+
+                if (message.params.event[0] === "cert_changed" && message.params.event[1])
+                    this.loadMessages()
+            } else {
                 $("#select_user").html("Select user")
+                $('#current_user_name').html("Please login first")
+                $("#current_user_avatar").html('<figure class="avatar" data-initial="TW"></figure>')
+            }
 
             if (message.params.event[0] == "file_done")
                 this.loadMessages()
